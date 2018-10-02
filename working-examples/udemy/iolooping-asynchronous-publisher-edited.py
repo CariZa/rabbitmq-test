5bd274a193bfa7681a66b6aa2f536ea8f19b5108
CariZa <liebenbc@yahoo.com>
2018-10-02 12:36:46 +0200

    Added more of the udemy examples.

---
+++ working-examples/udemy/iolooping-asynchronous-publisher-edited.py
@@ -0,0 +1,93 @@
+import pika
+import logging
+
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(name) -30s %(funcName) '
+                '-35s %(lineno) -5d: %(message)s')
+LOGGER = logging.getLogger(__name__)
+
+class publish_engine:
+
+    def __init__(self):
+        self._number_of_messages = 10
+        self._channel = None
+        self._connection = None
+        self._message_interval = 1
+        self._delivered_messages = 0
+        self._published_messages = 0
+
+    def on_open(self, connection):
+        # Invoked when the connection is open
+        print("Reached connection open \n")
+        self._channel = self._connection.channel(self.on_channel_open)
+        #connection.close()
+
+    def on_delivery_confirmation(self, method_frame):
+        confirmation_type = method_frame.method.NAME.split('.')[1].lower()
+        self._delivered_messages += 1
+        print("\n"
+            "=========================")
+        print(method_frame)
+        print("An %s has been received!!!" %confirmation_type)
+        print("Published %i messages and received delivery confirmation for %i messages " %(self._published_messages, self._delivered_messages) )
+        print("=========================")
+
+    def publish_message(self):
+        print("Now in publish message")
+        # while self._number_of_messages > 0:
+            # print(self._number_of_messages)
+
+        self._channel.basic_publish(exchange='',
+                            routing_key='orders_q',
+                            body='H' + str(self._number_of_messages),
+                            properties=pika.BasicProperties(content_type='text/plain',
+                                                    delivery_mode=2))
+
+        self._number_of_messages -= 1
+        self._published_messages += 1
+
+        if self._number_of_messages >= 0:
+            self.schedule_next_message()
+        else:
+            self._connection.close()
+
+    def schedule_next_message(self):
+        self._connection.add_timeout(self._message_interval, self.publish_message)
+
+    def on_channel_open(self, channel):
+        print("Reached channel open \n")
+        argument_list = {'x-queue-master-locator': 'random'}
+        self._channel.queue_declare(self.on_declare, queue='orders_q', durable=True, arguments=argument_list)
+
+    def on_declare(self, channel):
+        print("Now in on declare")
+        self._channel.confirm_delivery(self.on_delivery_confirmation)
+        #self.publish()
+        self.schedule_next_message()
+
+    def on_close(self, connection, reply_code, reply_message):
+        #This will be called on connection close
+        print("start on_close callback \n")
+        print(reply_code)
+        print(reply_message)
+        print("connection is being closed \n")
+
+    def run(self):
+    # Create our connection object, passing in the on_open method
+        logging.basicConfig(level=logging.ERROR, format=LOG_FORMAT)
+        credentials = pika.PlainCredentials('guest', 'guest')
+        # 54.200.201.1
+        parameters = pika.ConnectionParameters('localhost', 5672, '/', credentials, socket_timeout=300)
+        self._connection = pika.SelectConnection(parameters, on_open_callback=self.on_open)
+        self._connection.add_on_close_callback(self.on_close)
+        print("Script execution is done!! will start IO Loop")
+
+        try:
+            # Loop so we can communicate with RabbitMQ
+            self._connection.ioloop.start()
+        except KeyboardInterrupt:
+            # Gracefully close the connection
+            self._connection.close()
+
+if __name__ == '__main__':
+    engine = publish_engine()
+    engine.run()
